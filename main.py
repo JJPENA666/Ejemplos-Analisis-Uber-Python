3cf8d2c452ffe2ac9f7e47589a88756893c13368
JJPENA666 <hardjostar@gmail.com>
2022-07-14 17:23:25 -0500

    Subir

---
+++ main.py
@@ -0,0 +1,12 @@
+from pprint import pprint
+from account import Account
+from car import Car
+
+
+if __name__ == "__main__":
+    print ("Hola mundo")
+
+    car = Car("PBO5555", Account("Diego Yanez", "17215555555"))   
+    
+    print(vars(car))
+    print(vars(car.driver))
